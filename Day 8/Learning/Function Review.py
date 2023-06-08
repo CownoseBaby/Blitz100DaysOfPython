183fab3db34c5971ad9e1053ba03eac140474080
Blitz Dreamrider <57915618+hero760@users.noreply.github.com>
2023-06-08 16:38:03 -0500

    Day 8

---
+++ Day 8/Learning/Function Review.py
@@ -0,0 +1,20 @@
+# Simple Function
+# def greet():
+#     print("Hello World")
+#     print("I am a computer :)")
+#     print("Nice to meet you")
+# greet()
+#
+# Function that allows for input
+# def greet_with_name(name):
+#     print(f"Hello {name}")
+#     print("I am a computer :)")
+#     print("Nice to meet you")
+# greet_with_name("blitz")
+
+# Functions with more than 1 input
+def greet_with(name, location):
+    print(f"Hi there {name}")
+    print(f"How is the weather at {location}?")
+
+greet_with("Blitz", "STL")
