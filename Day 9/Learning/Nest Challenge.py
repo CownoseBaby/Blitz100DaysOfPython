353375b7362e7665971308e85b6fe7e4caeb4abf
Blitz Dreamrider <57915618+hero760@users.noreply.github.com>
2023-06-12 19:49:26 -0500

    Day 9

---
+++ Day 9/Learning/Nest Challenge.py
@@ -0,0 +1,8 @@
+travel_log = {
+    "France": {"cities_visited1": "Paris",
+               "cities_visited2": "Lille",
+               "cities_visited3": "Dijon"},
+    "Germany": ["Berlin", "Hamburg", "Stuttgart"],
+}
+
+print(travel_log)
